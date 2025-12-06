2a5b9e6444576ad42d8205294481ac51e1aae457
Aditya N Khatawkar <110993700+AdityaKhatawkar@users.noreply.github.com>
2025-12-06 20:26:29 +0530

    Update toaster.tsx

--- frontend/src/components/ui/toaster.tsx
+++ frontend/src/components/ui/toaster.tsx
@@ -1,35 +1,18 @@
-"use client"
+"use client";
 
-import { useToast } from "@/hooks/use-toast"
-import {
-  Toast,
-  ToastClose,
-  ToastDescription,
-  ToastProvider,
-  ToastTitle,
-  ToastViewport,
-} from "@/components/ui/toast"
+import { useToast } from "@/hooks/use-toast";
+import ToastItem from "@/components/ui/toast";
 
 export function Toaster() {
-  const { toasts } = useToast()
+  const { toasts, removeToast } = useToast();
 
   return (
-    <ToastProvider>
-      {toasts.map(function ({ id, title, description, action, ...props }) {
-        return (
-          <Toast key={id} {...props}>
-            <div className="grid gap-1">
-              {title && <ToastTitle>{title}</ToastTitle>}
-              {description && (
-                <ToastDescription>{description}</ToastDescription>
-              )}
-            </div>
-            {action}
-            <ToastClose />
-          </Toast>
-        )
-      })}
-      <ToastViewport />
-    </ToastProvider>
-  )
+    <div className="fixed z-50 top-4 right-4 flex flex-col gap-3 w-full max-w-sm pointer-events-none">
+      {toasts.map((toast) => (
+        <div key={toast.id} className="pointer-events-auto">
+          <ToastItem toast={toast} onRemove={removeToast} />
+        </div>
+      ))}
+    </div>
+  );
 }
